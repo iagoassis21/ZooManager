380772e6815cecfc441b798bc178153e049af1d4
Iago <iagoassis21@outlook.com>
2022-05-24 02:27:10 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  return data.employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
