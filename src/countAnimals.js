b5a974ce67fd0290b813b3a6de10b3f65add4d26
Iago <iagoassis21@outlook.com>
2022-05-25 17:17:03 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const contaAnimal = data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+    return contaAnimal;
+  }
+  const { specie, sex } = animal;
+  if (specie && sex) {
+    return species.find((element) => element.name === specie).residents
+      .filter((element) => element.sex === sex).length;
+  }
+
+  if (specie !== undefined) {
+    return species.find((element) => element.name === specie).residents.length;
+  }
 }
 
 module.exports = countAnimals;
